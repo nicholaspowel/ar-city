71af0c9939e56c234b403cc02f79f3cbc8d65b3c
Nick <nicholaspowel@users.noreply.github.com>
2017-08-31 11:45:29 -0400

    feat(FilterScreen): Begin creating skeleton of feature
    Beginning to create the visual outline of the feature
    
    BREAKING CHANGE: none
    
    none

--- screens/Map/FilterScreen.js
+++ screens/Map/FilterScreen.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ScrollView, StyleSheet, Text, TextInput, View, Button } from 'react-native';
-import * as Actions from '../../redux/actions';
+import * as Actions from '../../actions';
 import { NavigationActions } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Expo from 'expo';
 import firebase from 'firebase';
+import db from '../../db';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    phototags: state.phototags,
+  };
+};
 
 class FilterScreen extends Component {
-	
+
+  render(){
+    return(
+      <ScrollView>
+        <ScrollView>
+        </ScrollView>
+        <TextInput>
+        </TextInput>
+      </ScrollView>
+      )
+  }
 }
+
+export default FilterScreen;
+
+//need an option for favorites
+//need a selector for recent, popular, most voted, most faved
+//need to allow selection by tag (check marks?)
+//allow for different zoom levels?
+//change search radius
