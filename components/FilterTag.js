8dd0e38b24c4c8026cae6279ae99a8cecb4c888b
Nick <nicholaspowel@users.noreply.github.com>
2017-08-31 12:22:44 -0400

    refactor(FilterTagItem): Begin creating filter tag button component
    Creating the form for the tag buttons that will be used to select tags as filters
    
    BREAKING CHANGE: none
    
    none

---
+++ components/FilterTag.js
@@ -0,0 +1,14 @@
+import React from 'React';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
+
+
+class FilterTagItem extends React.Component {
+  
+  
+  render(){
+    return(
+      <Button>
+      </Button>
+      )
+  }
+}
